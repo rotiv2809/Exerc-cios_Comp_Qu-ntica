3833b69d48506bcf9c12f8ed76beed743b98e2c2
rotiv2809 <112286387+rotiv2809@users.noreply.github.com>
2024-03-11 09:16:32 -0300

    16.py

--- 16.py
+++ 16.py
@@ -1,22 +1,59 @@
-import numpy as np
-def encontrar_transposta_conjugada_dagger(matriz):
-    transposta = np.transpose(matriz)
-    conjugada = np.conjugate(matriz)
-    dagger = np.conjugate(transposta)
-    return transposta, conjugada, dagger
-n_linhas = int(input("Digite o número de linhas da matriz: "))
-n_colunas = int(input("Digite o número de colunas da matriz: "))
+def transposta(matriz):
+    if not matriz:
+        return []
+    linhas = len(matriz)
+    colunas = len(matriz[0])
+    
+    transposta = []
+    
+    for j in range(colunas):
+        linha = []
+        for i in range(linhas):
+            elemento = matriz[i][j]
+            linha.append(elemento)
+        transposta.append(linha)
+    
+    return transposta
 
-matriz_complexa = np.array([[complex(input(f"Digite o elemento ({i+1},{j+1}) da matriz (formato a+bj): ")) for j in range(n_colunas)] for i in range(n_linhas)])
-transposta, conjugada, dagger = encontrar_transposta_conjugada_dagger(matriz_complexa)
-print("\nMatriz Original:")
-print(matriz_complexa)
+def conjugada(matriz):
+    if not matriz:
+        return []
+    
+    linhas = len(matriz)
+    colunas = len(matriz[0])
+    
+    conjugada = []
+    
+    for i in range(linhas):
+        linha = []
+        for j in range(colunas):
+            elemento = matriz[i][j].conjugate()
+            linha.append(elemento)
+        conjugada.append(linha)
+    return conjugada    
 
-print("\nTransposta:")
-print(transposta)
+linhas = int(input("Digite o número de linhas da matriz: "))
+colunas = int(input("Digite o número de colunas da matriz: "))
 
-print("\nConjugada:")
-print(conjugada)
+matriz = []
 
-print("\nDagger:")
-print(dagger)
+print("Digite os elementos da linha")
+for i in range (linhas):
+    linha = []
+    for j in range (colunas):
+        elemento = complex(input(f"Insira o elemento [{i+1}][{j+1}]: "))
+        linha.append(elemento)
+    matriz.append(linha)
+    
+print("A matriz transposta será:")
+
+for i in range(len(transposta(matriz))):
+    print(transposta(matriz)[i])
+
+print("A matriz conjugada será:")
+for linha in conjugada(matriz):
+    print(linha)
+    
+print("A matriz dagger será:")
+for linha in transposta(conjugada(matriz)):
+    print(linha)
